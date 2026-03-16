2491d2a8fed3ca870f1550864d2912f3a1d5bb51
F Rodriguez <fr.rodmar@gmail.com>
2026-03-16 23:59:57 +0000

    feat: Replace PNG icons with SVG icons in PWA configuration and HTML.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons/icon-192.png', 'icons/icon-512.png'],
+      includeAssets: ['favicon.svg', 'icons/icon-192.svg'],
       manifest: {
         id: 'com.medicamentos.app',
         name: 'Gestor de Medicamentos',
@@ -21,19 +21,19 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icons/icon-192.png',
+            src: 'icons/icon-192.svg',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/svg+xml'
           },
           {
-            src: 'icons/icon-512.png',
+            src: 'icons/icon-192.svg',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/svg+xml'
           },
           {
-            src: 'icons/icon-512.png',
+            src: 'icons/icon-192.svg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
